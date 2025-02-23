4cd3f30c9f78d3db7542ce90f0947359d95fc29e
Pedro Rodrigues <pedro.pfxr@gmail.com>
2025-02-23 20:54:12 +0000

    Adding a telegram bot to issue notifications

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import io
+import asyncio
 import threading
-import time
-from telegram_bot import telegram_bot
-import numpy as np
-from flask import Flask, Response
+import telegram_bot
+from flask import Flask
 from ir_camera import ir_camera
 
 app = Flask(__name__)
-cam = ir_camera(app)
-app.run(host = '0.0.0.0', port = 5000, threaded=True)
+cam = ir_camera(app, notification = telegram_bot.send_message)
+
+
+# Run Flask
+def run_flask():
+    app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    # Run Flask and Telegram Bot in parallel
+    threading.Thread(target=run_flask, daemon=True).start()
+    telegram_bot.init()  # Runs the bot in the main thread
---
+++ telegram_bot.py
@@ -0,0 +1,50 @@
+import logging
+import asyncio
+from telegram import Update, Bot
+from telegram.request import HTTPXRequest
+from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+
+# Enable logging
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# All users that subscribe to notifications
+user_chat_ids = set()
+
+# Telegram Bot Token (replace with your actual token)
+TOKEN = ""
+
+#trequest = HTTPXRequest(connection_pool_size=20)
+#bot = Bot(token=TOKEN, request=trequest)
+bot = Bot(token=TOKEN)
+
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info(f"Received message: {update.message.text}")
+    await update.message.reply_text(update.message.text)
+
+# Telegram Bot Handlers
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Add chat id to users
+    user_chat_ids.add(update.message.chat.id)  # Save the chat_id of new users
+
+    await update.message.reply_text("Registered, you'll start receiving notifications")
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f"Hello, {update.effective_user.first_name}!")
+
+# Function to send a message without a command
+def send_message(message):
+    for chat_id in user_chat_ids:
+        try:
+            asyncio.run(bot.send_message(chat_id=chat_id, text=message))
+        except Exception as e:
+            print(f"Failed to send message to {chat_id}: {e}")
+
+def init():
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("hello", hello))
+    application.run_polling()
+
